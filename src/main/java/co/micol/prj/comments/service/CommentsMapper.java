5475e7973c43b0aea75478236f6eba0a7041f5c1
audghks1996 <audghks1996@naver.com>
2022-03-28 14:31:57 +0900

    자유게시판 댓글기능 구현

--- src/main/java/co/micol/prj/comments/service/CommentsMapper.java
+++ src/main/java/co/micol/prj/comments/service/CommentsMapper.java
@@ -3,7 +3,7 @@ package co.micol.prj.comments.service;
 import java.util.List;
 
 public interface CommentsMapper {
-	List<CommentsVO> commentsSelectList();
+	List<CommentsVO> commentsSelectList(int boardNum);
 	CommentsVO commentsSelect(CommentsVO vo);
 	int commentsInsert(CommentsVO vo);
 	int commentsUpdate(CommentsVO vo);
